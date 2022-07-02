4f0c84b600bfba4f09fbfa5a9deb625b8124a6e7
Unicorn Barf <nolan.spence7@gmail.com>
2022-07-01 22:18:18 -0700

    Finished Manager class test and the class js file

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,11 +1,22 @@
 const Manager = require('../lib/Manager');
 
-describe('Manager Class:', () => {
-    describe('Method return correct data:', () => {
-        it('Has')
+describe('Employee Class:', () => {
+    describe('It creates a new object:', () => {
+        it('Is a new instance of a Manager object created with a name, id, email, and office number', () => {
+            const obj = new Manager('Jasmine', 5, 'whynot@because.com', 69);
+            expect(obj).toBeInstanceOf(Manager);
+            expect(obj.name).toBe('Jasmine');
+            expect(obj.id).toBe(5);
+            expect(obj.email).toBe('whynot@because.com');
+            expect(obj.officeNum).toBe(69);
+        })
     })
-})
 
-describe('Manager Class', () => {
-    
-})
+    describe('getRole() methods overrides the Employee class method', () => {
+
+        it('Has a getRole() method that returns "Manager"', () => {
+            const obj = new Manager('Jasmine', 5, 'whynot@because.com', 69);
+            expect(obj.getRole()).toBe('Manager');
+        });
+    });
+});
